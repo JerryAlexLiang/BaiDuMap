ef6b6927e33dbe8bb2c07815eb47d69b49558ba0
JerryAlexLiang <1935990239@qq.com>
2016-12-13 21:40:37 +0800

    定位

--- baidu_map/src/main/java/com/example/baidu_map/MainActivity.java
+++ baidu_map/src/main/java/com/example/baidu_map/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button mNearbySearchBtn;
     private Button mBusLineSearchBtn;
     private Button mRoutePlanBtn;
+    private Button mLocationBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +48,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mNearbySearchBtn = (Button) findViewById(R.id.nearby_search_btn);
         mBusLineSearchBtn = (Button) findViewById(R.id.busLine_search_btn);
         mRoutePlanBtn = (Button) findViewById(R.id.route_plan_btn);
+        mLocationBtn = (Button) findViewById(R.id.location_btn);
         //设置点击监听事件
         commonMap.setOnClickListener(this);
         satelliteMap.setOnClickListener(this);
@@ -61,6 +63,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mNearbySearchBtn.setOnClickListener(this);
         mBusLineSearchBtn.setOnClickListener(this);
         mRoutePlanBtn.setOnClickListener(this);
+        mLocationBtn.setOnClickListener(this);
     }
 
 
@@ -71,73 +74,90 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     @Override
     public void onClick(View view) {
+        Intent intent = new Intent();
         switch (view.getId()) {
             case R.id.common_map:
-                Intent intent = new Intent(MainActivity.this, CommonMap.class);
-                startActivity(intent);
+//                Intent intent = new Intent(MainActivity.this, CommonMap.class);
+//                startActivity(intent);
+                intent.setClass(MainActivity.this,CommonMap.class);
                 break;
 
             case R.id.satellite_map:
-                Intent intent1 = new Intent(MainActivity.this, SatelliteMap.class);
-                startActivity(intent1);
+//                Intent intent1 = new Intent(MainActivity.this, SatelliteMap.class);
+//                startActivity(intent1);
+                intent.setClass(MainActivity.this,SatelliteMap.class);
                 break;
 
             case R.id.empty_map:
-                Intent intent2 = new Intent(MainActivity.this, EmptyMap.class);
-                startActivity(intent2);
+//                Intent intent2 = new Intent(MainActivity.this, EmptyMap.class);
+//                startActivity(intent2);
+                intent.setClass(MainActivity.this,EmptyMap.class);
                 break;
 
             case R.id.traffic_map:
-                Intent intent3 = new Intent(MainActivity.this, TrafficMap.class);
-                startActivity(intent3);
+//                Intent intent3 = new Intent(MainActivity.this, TrafficMap.class);
+//                startActivity(intent3);
+                intent.setClass(MainActivity.this,TrafficMap.class);
                 break;
 
             case R.id.hot_map_btn:
-                Intent intent4 = new Intent(MainActivity.this,HotMap.class);
-                startActivity(intent4);
+//                Intent intent4 = new Intent(MainActivity.this,HotMap.class);
+//                startActivity(intent4);
+                intent.setClass(MainActivity.this,HotMap.class);
                 break;
 
             case R.id.central_map_btn:
-                Intent intent5 = new Intent(MainActivity.this,CentralMap.class);
-                startActivity(intent5);
+//                Intent intent5 = new Intent(MainActivity.this,CentralMap.class);
+//                startActivity(intent5);
+                intent.setClass(MainActivity.this,CentralMap.class);
                 break;
 
             case R.id.marker_map_btn:
-                Intent intent6 = new Intent(MainActivity.this,MarkerMap.class);
-                startActivity(intent6);
+//                Intent intent6 = new Intent(MainActivity.this,MarkerMap.class);
+//                startActivity(intent6);
+                intent.setClass(MainActivity.this,MarkerMap.class);
                 break;
 
             case R.id.text_options:
-                Intent intent7 = new Intent(MainActivity.this,TextOptionsMap.class);
-                startActivity(intent7);
+//                Intent intent7 = new Intent(MainActivity.this,TextOptionsMap.class);
+//                startActivity(intent7);
+                intent.setClass(MainActivity.this,TextOptionsMap.class);
                 break;
 
             case R.id.city_search_btn:
-                Intent intent8 = new Intent(MainActivity.this,CitySearchMap.class);
-                startActivity(intent8);
+//                Intent intent8 = new Intent(MainActivity.this,CitySearchMap.class);
+//                startActivity(intent8);
+                intent.setClass(MainActivity.this,CitySearchMap.class);
                 break;
 
             case R.id.range_search_btn:
-                Intent intent9 = new Intent(MainActivity.this,RangeSearchMap.class);
-                startActivity(intent9);
+//                Intent intent9 = new Intent(MainActivity.this,RangeSearchMap.class);
+//                startActivity(intent9);
+                intent.setClass(MainActivity.this,RangeSearchMap.class);
                 break;
 
             case R.id.nearby_search_btn:
-                Intent intent10 = new Intent(MainActivity.this,NearbySearchMap.class);
-                startActivity(intent10);
+//                Intent intent10 = new Intent(MainActivity.this,NearbySearchMap.class);
+//                startActivity(intent10);
+                intent.setClass(MainActivity.this,NearbySearchMap.class);
                 break;
 
             case R.id.busLine_search_btn:
-                Intent intent11 = new Intent(MainActivity.this,BusLineSearchMap.class);
-                startActivity(intent11);
+//                Intent intent11 = new Intent(MainActivity.this,BusLineSearchMap.class);
+//                startActivity(intent11);
+                intent.setClass(MainActivity.this,BusLineSearchMap.class);
                 break;
 
             case R.id.route_plan_btn:
-                Intent intent12 = new Intent(MainActivity.this,RoutePlanMap.class);
-                startActivity(intent12);
+//                Intent intent12 = new Intent(MainActivity.this,RoutePlanMap.class);
+//                startActivity(intent12);
+                intent.setClass(MainActivity.this,RoutePlanMap.class);
                 break;
 
-
+            case R.id.location_btn:
+                intent.setClass(MainActivity.this,LocationMap.class);
+                break;
         }
+        startActivity(intent);
     }
 }
