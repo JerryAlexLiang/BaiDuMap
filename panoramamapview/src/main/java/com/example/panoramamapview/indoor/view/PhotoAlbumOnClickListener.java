52b54001e12e71047f4b9791cde35998c1547e8b
JerryAlexLiang <1935990239@qq.com>
2017-07-17 15:00:45 +0800

    百度地图实现全景实景官方demo

---
+++ panoramamapview/src/main/java/com/example/panoramamapview/indoor/view/PhotoAlbumOnClickListener.java
@@ -0,0 +1,8 @@
+package com.example.panoramamapview.indoor.view;
+
+
+import com.example.panoramamapview.indoor.model.AlbumPicInfo;
+
+public interface PhotoAlbumOnClickListener {
+    void onItemClicked(AlbumPicInfo picInfo);
+}
